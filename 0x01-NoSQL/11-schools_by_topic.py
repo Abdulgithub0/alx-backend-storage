f30ef1d4e2ba5523d0bc61a4f0f226bcdf8cc00b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-19 16:25:25 +0100

    perform docs search based on topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""function that returns the list of school having a specific topic:
+"""function that return the list of school having a specific topic:
 
     Prototype: def schools_by_topic(mongo_collection, topic):
     mongo_collection will be the pymongo collection object
