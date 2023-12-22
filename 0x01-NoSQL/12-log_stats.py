c1f2f64f74410a452bd32e1d516ab06e938eb7e8
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-22 10:49:39 +0100

    web traffic logger

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-"""script that provides some stats about Nginx logs stored in MongoDB"""
+'''script that provides some stats about Nginx logs stored in MongoDB'''
 
 from pymongo import MongoClient
 
 
-def stat_logger():
-    """log the web traffic data store on the mongodb"""
+if __name__ == '__main__':
+    '''log the web traffic data store on the mongodb'''
     nginx = MongoClient().logs.nginx
     print(nginx.count_documents({}), ' logs\nMethods:')
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
@@ -14,6 +14,3 @@ def stat_logger():
         print('\tmethod {}: {}'.format(val,
               nginx.count_documents({'method': val})))
     print(nginx.count_documents({'path': '/status'}), ' status check')
-
-
-stat_logger()
