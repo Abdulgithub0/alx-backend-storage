43556e79b7108cb26a7f71b56f138410ea005bf1
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-22 22:47:36 +0100

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,9 @@
 """implementation of class cache"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
+
+anytype = Union[str, bytes, int, float]
 
 
 class Cache:
@@ -14,8 +16,21 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: anytype) -> str:
         '''store data using random generated key'''
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        self._key = str(uuid.uuid4())
+        self._redis.set(self._key, data)
+        return self._key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> anytype:
+        '''retrive store data from redis db'''
+        data = self._redis.get(key)
+        return fn(data) if fn and data else data
+
+    def get_str(self) -> str:
+        '''convert the retrive binary data to string'''
+        return self.get(self._key, str)
+
+    def get_int(self) -> int:
+        '''convert the retrive binary data to int'''
+        return self.get(self._key, int)
