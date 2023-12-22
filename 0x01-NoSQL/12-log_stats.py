243e4be014706b86c728df0f98933ea0a8e941d3
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-12-22 10:58:17 +0100

    web traffic logger

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,11 +9,10 @@ if __name__ == '__main__':
     '''log the web traffic data store on the mongodb'''
     nginx = MongoClient().logs.nginx
     doc = nginx.count_documents({})
-    if not doc
-        return
-    print(doc, ' logs\nMethods:')
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for val in methods:
-        print('\tmethod {}: {}'.format(val,
-              nginx.count_documents({'method': val})))
-    print(nginx.count_documents({'path': '/status'}), ' status check')
+    if doc:
+        print(doc, ' logs\nMethods:')
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        for val in methods:
+            print('\tmethod {}: {}'.format(val,
+                  nginx.count_documents({'method': val})))
+        print(nginx.count_documents({'path': '/status'}), ' status check')
